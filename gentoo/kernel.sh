35575c4c5a094e9462000441d90c6c4366fedc0d
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2022-07-15 10:48:33 -0300

    adding efficiency to the kernel script

--- gentoo/kernel.sh
+++ gentoo/kernel.sh
@@ -2,6 +2,6 @@
 
 # Script made for building the kernel with initramfs
 # It will select the kernel selected with "eselect kernel"
-cd /usr/src/linux && make clean && make -j11 && make modules_install install && \
-    genkernel --kernel-config=/usr/src/linux/.config --microcode --makeopts=-j11 initramfs && \
+cd /usr/src/linux && make clean && make -j6 && make modules_install install && \
+    genkernel --kernel-config=/usr/src/linux/.config --microcode initramfs && \
     grub-mkconfig -o /boot/grub/grub.cfg && emerge @module-rebuild
