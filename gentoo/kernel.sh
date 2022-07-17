c5ef0405c7148b323743aea0db5cddda173ea9ee
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2022-07-17 12:15:32 -0300

    testing out --microcode-initramfs tag for genkernel

--- gentoo/kernel.sh
+++ gentoo/kernel.sh
@@ -3,5 +3,5 @@
 # Script made for building the kernel with initramfs
 # It will select the kernel selected with "eselect kernel"
 cd /usr/src/linux && make clean && make -j6 && make modules_install install && \
-    genkernel --kernel-config=/usr/src/linux/.config --microcode initramfs && \
+    genkernel --kernel-config=/usr/src/linux/.config --microcode-initramfs initramfs && \
     grub-mkconfig -o /boot/grub/grub.cfg && emerge @module-rebuild
