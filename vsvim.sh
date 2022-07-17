3e714cd903a86a18791a5d8f173c0f73d569078f
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2022-07-17 12:13:59 -0300

    dwm compatibility for the script

--- vsvim.sh
+++ vsvim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This is a script to bind Caps Lock to Escape while in vscodium.
-# It is recommended to put it in $USER/.local/bin
+# It is recommended to put it in $USER/.local/bin or /usr/local/bin for dwm compatibility.
 setxkbmap -option caps:escape
 vscodium -w; setxkbmap -option
 
